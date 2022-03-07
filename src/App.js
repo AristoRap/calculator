711d1c5fcd0e619595bd2797490409b3883c4d02
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-03-08 00:46:09 +0100

    added sass; first draft of calc screen and operators

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Calculator from "./components/calculator/Calculator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hey! I created a react boilerplate.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calculator/>
     </div>
   );
 }
---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import "./Calculator.scss"
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <div className="calculator-wrapper">
+        <div className="calculator">
+          <div className="calculator-screen">
+            <div className="calculator-result">13</div>
+            <div className="calculator-current">6+7</div>
+          </div>
+            <div className="calculator-operators">
+              <button>C</button>
+              <button>DEL</button>
+            </div>
+              <button>÷</button>
+              <button>x</button>
+              <button>+</button>
+              <button>-</button>
+        </div>
+      </div>
+    )
+  }
+}
