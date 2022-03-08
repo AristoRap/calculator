9714c9c761bccce72cb38ed92b44cf773ef11599
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-03-08 17:07:34 +0100

    added basic calc styling

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -3,22 +3,33 @@ import "./Calculator.scss"
 
 export default class Calculator extends Component {
   render() {
-    return (
-      <div className="calculator-wrapper">
-        <div className="calculator">
-          <div className="calculator-screen">
-            <div className="calculator-result">13</div>
-            <div className="calculator-current">6+7</div>
-          </div>
-            <div className="calculator-operators">
-              <button>C</button>
-              <button>DEL</button>
-              <button>÷</button>
-              <button>x</button>
-              <button>+</button>
-              <button>-</button>
-            </div>
+  return (
+      <div className="calculator">
+        <div className="calculator-screen">
+          <div className="calculator-result">13</div>
+          <div className="calculator-current">6+7</div>
         </div>
+          <div className="calculator-buttons">
+            <button className="span-two">AC</button>
+            <button>DEL</button>
+            <button>÷</button>
+
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>x</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>-</button>
+            <button>.</button>
+            <button>0</button>
+            <button className="span-two">=</button>
+          </div>
       </div>
     )
   }
