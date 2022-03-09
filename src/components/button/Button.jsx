c7281c59311be9287e0b4e42212a61bf52235612
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-03-09 23:24:26 +0100

    refactored onclick in button components

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -5,8 +5,7 @@ export default class Button extends Component {
   render() {
     return (
       <button
-        value={this.props.value}
-        onClick={(e) => this.props.onClick(e.target.value)}
+        onClick={() => this.props.onClick(this.props.value)}
         className={this.props.class && this.props.class}
       >
         {this.props.value}
